21d2ff049e11f83a4a18d67e285f9efc77500e7e
jeensh <jeensh25@gmail.com>
2023-08-23 23:45:04 +0900

    init : 프로젝트 생성

---
+++ j_log/src/main/java/com/jeensh/j_log/JLogApplication.java
@@ -0,0 +1,13 @@
+package com.jeensh.j_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JLogApplication.class, args);
+	}
+
+}
