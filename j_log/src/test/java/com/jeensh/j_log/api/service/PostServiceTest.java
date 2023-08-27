31baf1eb416fefbbb9695f1f383cbb6e8203e5ca
jeensh <jeensh25@gmail.com>
2023-08-27 17:18:10 +0900

    test[PostServie] : 테스트 코드 작성

---
+++ j_log/src/test/java/com/jeensh/j_log/api/service/PostServiceTest.java
@@ -0,0 +1,39 @@
+package com.jeensh.j_log.api.service;
+
+import com.jeensh.j_log.api.domain.Post;
+import com.jeensh.j_log.api.repository.PostRepository;
+import com.jeensh.j_log.api.request.PostCreate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("글 작성")
+    void writePostTest() {
+        //given
+        PostCreate postCreate = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        //when
+        postService.write(postCreate);
+
+        //then
+        assertThat(postRepository.count()).isEqualTo(1L);
+        Post post = postRepository.findAll().get(0);
+        assertThat(post.getTitle()).isEqualTo(postCreate.getTitle());
+        assertThat(post.getContent()).isEqualTo(postCreate.getContent());
+    }
+}
