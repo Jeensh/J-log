f10bad25a9c20c0f119a7aac570e06f7dcd91565
jeensh <jeensh25@gmail.com>
2023-09-05 22:49:25 +0900

    feat : 게시글 작성, 게시글 목록 조회 화면 추가

--- j_log/front/src/views/HomeView.vue
+++ j_log/front/src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import axios from "axios";
+import constant from "@/stores/constant";
+import { ref } from "vue";
+
+const posts : any = ref([]);
+
+axios.get(constant.J_LOG_POST_API + "?page=1&size=10").then(response => {
+  response.data.forEach((post: any) => {
+    posts.value.push(post);
+  });
+})
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <div>{{ post.title }}</div>
+      <div>{{ post.content }}</div>
+    </li>
+  </ul>
 </template>
+
+<style></style>
