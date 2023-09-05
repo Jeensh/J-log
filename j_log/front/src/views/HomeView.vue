dda8692db666ae7cfe0d6d787e880a1c17d1841c
jeensh <jeensh25@gmail.com>
2023-09-05 23:42:48 +0900

    feat : 게시글 내용 확인 화면 추가

--- j_log/front/src/views/HomeView.vue
+++ j_log/front/src/views/HomeView.vue
@@ -2,6 +2,9 @@
 import axios from "axios";
 import constant from "@/stores/constant";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const posts : any = ref([]);
 
@@ -11,13 +14,18 @@ axios.get(constant.J_LOG_POST_API + "?page=1&size=10").then(response => {
   });
 })
 
+const moveToRead = function() {
+  router.push({ name: "read" });
+};
+
 </script>
 
 <template>
   <ul>
     <li v-for="post in posts" :key="post.id">
-      <div>{{ post.title }}</div>
-      <div>{{ post.content }}</div>
+      <div>
+        <router-link :to="{name: 'read', params: {postId: post.id} }">{{ post.title }}</router-link>
+      </div>
     </li>
   </ul>
 </template>
---
+++ j_log/front/src/views/ReadView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import constant from '@/stores/constant';
+import axios from "axios"
+
+const props = defineProps({
+    postId : {
+        type: [Number, String],
+        require: true,
+    }
+});
+
+const post = ref({
+    id: 0,
+    title: "",
+    content: "",
+});
+
+onMounted(() =>{
+    axios.get(constant.J_LOG_POST_API + `/${props.postId}`).then((response) =>{
+        post.value = response.data;
+    });
+});
+
+</script>
+
+<template>
+    <h2>{{ post.title }}</h2>
+    <div>{{ post.content }}</div>
+</template>
+
+<style>
+</style>
