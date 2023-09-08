2644ac4697ac87c88e2a3a2c9a2108bbd6edc227
jeensh <jeensh25@gmail.com>
2023-09-09 04:51:41 +0900

    chore : AWS front server CORS 허용

---
+++ j_log/src/main/java/com/jeensh/j_log/api/config/CorsAllowOrigin.java
@@ -0,0 +1,6 @@
+package com.jeensh.j_log.api.config;
+
+public final class CorsAllowOrigin {
+    public static final String	LOCAL_FRONT	= "http://localhost:3000";
+    public static final String	AWS_FRONT	= "http://ec2-43-201-50-202.ap-northeast-2.compute.amazonaws.com:80";
+}
--- j_log/src/main/java/com/jeensh/j_log/api/config/WebConfig.java
+++ j_log/src/main/java/com/jeensh/j_log/api/config/WebConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static com.jeensh.j_log.api.config.CorsAllowOrigin.*;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(LOCAL_FRONT, AWS_FRONT)
                 .allowedMethods("*");
+
     }
 }
