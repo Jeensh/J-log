ba2f4a7665494df7a2b8eb9f4fd49ab53cea4adc
jeensh <jeensh25@gmail.com>
2023-09-09 15:02:32 +0900

    fix : 게시글 api 서버 url 스키마 누락 해결

--- j_log/front/src/stores/constant.ts
+++ j_log/front/src/stores/constant.ts
@@ -1,4 +1,4 @@
 export default {
     // J_LOG_POST_API: "http://localhost:8080/posts"
-    J_LOG_POST_API: "ec2-43-201-50-202.ap-northeast-2.compute.amazonaws.com:8080/posts"
+    J_LOG_POST_API: "http://ec2-43-201-50-202.ap-northeast-2.compute.amazonaws.com:8080/posts"
 }
